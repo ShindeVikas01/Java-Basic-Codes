c350709329c23ebd121783ad02542a2e65ff1270
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-04-01 17:35:46 +0530

    [ADDED]:[Vikas] Swap two number

---
+++ P2Swap2num.java
@@ -0,0 +1,10 @@
+class P2Swap2num{
+	public static void main(String args[])
+	{
+		int num1=50,num2=60;
+		num1=num1+num2;
+		num2=num1-num2;
+		num1=num1-num2;
+		System.out.println("num1="+num1+"\tnum2="+num2);
+	}
+}
