26514c51c0812db1c41ec13bd66327065ab7fcb8
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-04-01 17:38:38 +0530

    [ADDED]:[Vikas] Find even or odd number

---
+++ P4EvenOdd.java
@@ -0,0 +1,15 @@
+class P4EvenOdd{
+	public static void main(String args[])
+	{
+		int num;
+		num=Integer.parseInt(args[0]);
+		if(num%2==0)
+		{
+			System.out.println("Given number is Even number");
+		}
+		else
+		{
+			System.out.println("Given number is Odd numer");
+		}
+	}
+}
