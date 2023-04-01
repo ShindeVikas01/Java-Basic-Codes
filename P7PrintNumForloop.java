e934453c82f7bcfbcc7108defeb599c91aaba0bd
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-04-01 17:48:31 +0530

    [ADDED]:[Vikas] Print the numbers between given two numbers

---
+++ P7PrintNumForloop.java
@@ -0,0 +1,12 @@
+class P7PrintNumForloop{
+	public static void main(String args[])
+	{
+		int num1,num2,i;
+		num1=Integer.parseInt(args[0]);
+		num2=Integer.parseInt(args[1]);
+		for(i=num1;i<=num2;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
